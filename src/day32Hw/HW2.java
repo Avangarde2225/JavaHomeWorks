19bc471d56536abd04f41007c699463ff8550c2c
Serkan_PC <serdar2225@gmail.com>
2019-11-17 21:54:38 -0500

    Day 32 Homework 1

---
+++ src/day32Hw/HW2.java
@@ -0,0 +1,23 @@
+package day32Hw;
+
+public class HW2 {
+    // create class Food
+    // add at least 5 attributes
+    // add at least 2 methods
+    //
+}
+class food {
+    boolean isVegetarian;
+    boolean isDelicious;
+    boolean isExpensive;
+    int price;
+    String name;
+
+
+    public static void eatFood() {}
+
+    public static void howToCook () {}
+}
+
+
+
